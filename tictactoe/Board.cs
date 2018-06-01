272dae1be2f33a07922f80dfa7e6553d33ca7758
Trey Mosley <iiimosley@gmail.com>
2018-05-31 23:09:51 -0500

    horizontal rows added to table

--- tictactoe/Board.cs
+++ tictactoe/Board.cs
@@ -4,8 +4,8 @@ namespace tictactoe
 {
   class Board
   {
+    static string straightLine = new String('-', 11);
     public static string CurrentStatus = 
-    $" {RowMatrix.Row1[0]} | {RowMatrix.Row1[1]} | {RowMatrix.Row1[2]} {Environment.NewLine} {RowMatrix.Row2[0]} | {RowMatrix.Row2[1]} | {RowMatrix.Row2[2]} {Environment.NewLine} {RowMatrix.Row3[0]} | {RowMatrix.Row3[1]} | {RowMatrix.Row3[2]} {Environment.NewLine}";
-
+    $" {RowMatrix.Row1[0]} | {RowMatrix.Row1[1]} | {RowMatrix.Row1[2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {RowMatrix.Row2[0]} | {RowMatrix.Row2[1]} | {RowMatrix.Row2[2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {RowMatrix.Row3[0]} | {RowMatrix.Row3[1]} | {RowMatrix.Row3[2]} {Environment.NewLine}";
   }
 }
