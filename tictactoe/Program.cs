93e7d9ba137f0277183de934bcd509c07bcd942b
Trey Mosley <iiimosley@gmail.com>
2018-06-01 01:19:55 -0500

    modularize print; recall on switch

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -5,82 +5,86 @@ namespace tictactoe
     class Program
     {
         public static char CurrentPlayer = 'X';
+        public static int printTable(string status)
+        {
+            Console.WriteLine(status);
+            Console.WriteLine($"Current Player: {CurrentPlayer}");
+            Console.Write("Mark a spot (1-9): ");
+            return int.Parse(Console.ReadLine());
+        }
         static void Main()
         {
             try{
-            Console.WriteLine(Board.CurrentStatus);
-            Console.WriteLine($"Current Player: {CurrentPlayer}");
-            Console.Write("Mark a spot (1-9):");
-            int input = int.Parse(Console.ReadLine());
-            do 
-            {
-                switch (input)
+                int input = printTable(Board.CurrentStatus);
+                do 
                 {
-                    case 1:
+                    switch (input)
                     {
-                        Console.WriteLine($"You entered {input}");
-                        Board.Matrix[0][0] = CurrentPlayer;
-                        Main();
-                        break;
+                        case 1:
+                        {
+                            Console.WriteLine($"You entered {input}");
+                            Board.Matrix[0][0] = CurrentPlayer;
+                            printTable(Board.CurrentStatus);
+                            break;
+                        }
+                        case 2:
+                        {
+                            Console.WriteLine($"You entered {input}");
+                            Board.Matrix[0][1] = CurrentPlayer;
+                            printTable(Board.CurrentStatus);
+                            break;
+                        }
+                        case 3:
+                        {
+                            Console.WriteLine($"You entered {input}");
+                            Board.Matrix[0][2] = CurrentPlayer;
+                            printTable(Board.CurrentStatus);
+                            break;
+                        }
+                        case 4:
+                        {
+                            Console.WriteLine($"You entered {input}");
+                            Board.Matrix[1][0] = CurrentPlayer;
+                            printTable(Board.CurrentStatus);
+                            break;
+                        }
+                        case 5:
+                        {
+                            Console.WriteLine($"You entered {input}");
+                            Board.Matrix[1][1] = CurrentPlayer;
+                            printTable(Board.CurrentStatus);
+                            break;
+                        }
+                        case 6:
+                        {
+                            Console.WriteLine($"You entered {input}");
+                            Board.Matrix[1][2] = CurrentPlayer;
+                            printTable(Board.CurrentStatus);
+                            break;
+                        }
+                        case 7:
+                        {
+                            Console.WriteLine($"You entered {input}");
+                            Board.Matrix[2][0] = CurrentPlayer;
+                            printTable(Board.CurrentStatus);
+                            break;
+                        }
+                        case 8:
+                        {
+                            Console.WriteLine($"You entered {input}");
+                            Board.Matrix[2][1] = CurrentPlayer;
+                            printTable(Board.CurrentStatus);
+                            break;
+                        }
+                        case 9:
+                        {
+                            Console.WriteLine($"You entered {input}");
+                            Board.Matrix[2][2] = CurrentPlayer;
+                            printTable(Board.CurrentStatus);
+                            break;
+                        }
                     }
-                    case 2:
-                    {
-                        Console.WriteLine($"You entered {input}");
-                        Board.Matrix[0][1] = CurrentPlayer;
-                        Main();
-                        break;
-                    }
-                    case 3:
-                    {
-                        Console.WriteLine($"You entered {input}");
-                        Board.Matrix[0][2] = CurrentPlayer;
-                        Main();
-                        break;
-                    }
-                    case 4:
-                    {
-                        Console.WriteLine($"You entered {input}");
-                        Board.Matrix[1][0] = CurrentPlayer;
-                        Main();
-                        break;
-                    }
-                    case 5:
-                    {
-                        Console.WriteLine($"You entered {input}");
-                        Board.Matrix[1][1] = CurrentPlayer;
-                        Main();
-                        break;
-                    }
-                    case 6:
-                    {
-                        Console.WriteLine($"You entered {input}");
-                        Board.Matrix[1][2] = CurrentPlayer;
-                        Main();
-                        break;
-                    }
-                    case 7:
-                    {
-                        Console.WriteLine($"You entered {input}");
-                        Board.Matrix[2][0] = CurrentPlayer;
-                        Main();
-                        break;
-                    }
-                    case 8:
-                    {
-                        Console.WriteLine($"You entered {input}");
-                        Board.Matrix[2][1] = CurrentPlayer;
-                        Main();
-                        break;
-                    }
-                    case 9:
-                    {
-                        Console.WriteLine($"You entered {input}");
-                        Board.Matrix[2][2] = CurrentPlayer;
-                        Main();
-                        break;
-                    }
-                }
-            }while(input < 10 && input < 0);
+                } while(input < 10 && input < 0);
             }
             catch(FormatException)
             {
