3f3190cd12e80e5f30253265479d861fd96af773
Trey Mosley <iiimosley@gmail.com>
2018-05-31 23:04:19 -0500

    basic board printing

--- tictactoe/Board.cs
+++ tictactoe/Board.cs
@@ -4,7 +4,8 @@ namespace tictactoe
 {
   class Board
   {
-    public static string CurrentStatus = "this will be a board";
+    public static string CurrentStatus = 
+    $" {RowMatrix.Row1[0]} | {RowMatrix.Row1[1]} | {RowMatrix.Row1[2]} {Environment.NewLine} {RowMatrix.Row2[0]} | {RowMatrix.Row2[1]} | {RowMatrix.Row2[2]} {Environment.NewLine} {RowMatrix.Row3[0]} | {RowMatrix.Row3[1]} | {RowMatrix.Row3[2]} {Environment.NewLine}";
 
   }
 }
--- tictactoe/RowMatrix.cs
+++ tictactoe/RowMatrix.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 namespace tictactoe
 {
   class RowMatrix
   {
-
+    public static char[] Row1 = new char[] { ' ', ' ', ' ' };
+    public static char[] Row2 = new char[] { ' ', ' ', ' ' };
+    public static char[] Row3 = new char[] { ' ', ' ', ' ' };
+    // public static List<char>[] Table = new List<char>[] { Row1, Row2, Row3 };
   }
 }
