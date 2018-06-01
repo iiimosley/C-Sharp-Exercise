efedd4613a0b9c2e358e9d37d3a98b3bc997a19d
Trey Mosley <iiimosley@gmail.com>
2018-06-01 00:26:22 -0500

    initial prompt created

--- tictactoe/Board.cs
+++ tictactoe/Board.cs
@@ -8,6 +8,6 @@ namespace tictactoe
     static string straightLine = new String('-', 11);
     public static List<List<char>> Matrix = RowMatrix.Table;
     public static string CurrentStatus = 
-    $" {Matrix[0][0]} | {Matrix[0][1]} | {Matrix[0][2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {Matrix[1][0]} | {Matrix[1][1]} | {Matrix[1][2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {Matrix[2][0]} | {Matrix[2][1]} | {Matrix[2][2]} {Environment.NewLine}";
+    $"{Environment.NewLine} {Matrix[0][0]} | {Matrix[0][1]} | {Matrix[0][2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {Matrix[1][0]} | {Matrix[1][1]} | {Matrix[1][2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {Matrix[2][0]} | {Matrix[2][1]} | {Matrix[2][2]} {Environment.NewLine}";
   }
 }
--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -4,9 +4,13 @@ namespace tictactoe
 {
     class Program
     {
+        public static char CurrentPlayer = 'X';
         static void Main()
         {
             Console.WriteLine(Board.CurrentStatus);
+            Console.WriteLine($"Current Player: {CurrentPlayer}");
+            Console.Write("Mark a spot (1-9):");
+            Console.ReadLine();
         }
     }
 }
