e5322425b48ec2dbf066c90f45a1bf8bf0ea76f4
Trey Mosley <iiimosley@gmail.com>
2018-06-02 21:49:55 -0500

    prints first move

--- tictactoe/Board.cs
+++ tictactoe/Board.cs
@@ -6,8 +6,10 @@ namespace tictactoe
   class Board
   {
     static string straightLine = new String('-', 11);
-    public static char[][] Matrix = RowMatrix.Table;
-    public static string CurrentStatus = 
-    $"{Environment.NewLine} {Matrix[0][0]} | {Matrix[0][1]} | {Matrix[0][2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {Matrix[1][0]} | {Matrix[1][1]} | {Matrix[1][2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {Matrix[2][0]} | {Matrix[2][1]} | {Matrix[2][2]} {Environment.NewLine}";
+    
+    public static string CurrentStatus (char[][] Matrix)
+    { 
+    return $"{Environment.NewLine} {Matrix[0][0]} | {Matrix[0][1]} | {Matrix[0][2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {Matrix[1][0]} | {Matrix[1][1]} | {Matrix[1][2]} {Environment.NewLine}{straightLine}{Environment.NewLine} {Matrix[2][0]} | {Matrix[2][1]} | {Matrix[2][2]} {Environment.NewLine}";
+    }
   }
 }
--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -15,7 +15,7 @@ namespace tictactoe
         public static void Main()
         {
             try{
-                int input = printTable(Board.CurrentStatus);
+                int input = printTable(Board.CurrentStatus(RowMatrix.Table));
                 Console.WriteLine(input);
                 do 
                 {
@@ -24,64 +24,64 @@ namespace tictactoe
                         case 1:
                         {
                             Console.WriteLine($"You entered {input}");
-                            Board.Matrix[0][0] = CurrentPlayer;
-                            printTable(Board.CurrentStatus);
+                            RowMatrix.Table[0][0] = CurrentPlayer;
+                            printTable(Board.CurrentStatus(RowMatrix.Table));
                             break;
                         }
                         case 2:
                         {
                             Console.WriteLine($"You entered {input}");
-                            Board.Matrix[0][1] = CurrentPlayer;
-                            printTable(Board.CurrentStatus);
+                            RowMatrix.Table[0][1] = CurrentPlayer;
+                            printTable(Board.CurrentStatus(RowMatrix.Table));
                             break;
                         }
                         case 3:
                         {
                             Console.WriteLine($"You entered {input}");
-                            Board.Matrix[0][2] = CurrentPlayer;
-                            printTable(Board.CurrentStatus);
+                            RowMatrix.Table[0][2] = CurrentPlayer;
+                            printTable(Board.CurrentStatus(RowMatrix.Table));
                             break;
                         }
                         case 4:
                         {
                             Console.WriteLine($"You entered {input}");
-                            Board.Matrix[1][0] = CurrentPlayer;
-                            printTable(Board.CurrentStatus);
+                            RowMatrix.Table[1][0] = CurrentPlayer;
+                            printTable(Board.CurrentStatus(RowMatrix.Table));
                             break;
                         }
                         case 5:
                         {
                             Console.WriteLine($"You entered {input}");
-                            Board.Matrix[1][1] = CurrentPlayer;
-                            printTable(Board.CurrentStatus);
+                            RowMatrix.Table[1][1] = CurrentPlayer;
+                            printTable(Board.CurrentStatus(RowMatrix.Table));
                             break;
                         }
                         case 6:
                         {
                             Console.WriteLine($"You entered {input}");
-                            Board.Matrix[1][2] = CurrentPlayer;
-                            printTable(Board.CurrentStatus);
+                            RowMatrix.Table[1][2] = CurrentPlayer;
+                            printTable(Board.CurrentStatus(RowMatrix.Table));
                             break;
                         }
                         case 7:
                         {
                             Console.WriteLine($"You entered {input}");
-                            Board.Matrix[2][0] = CurrentPlayer;
-                            printTable(Board.CurrentStatus);
+                            RowMatrix.Table[2][0] = CurrentPlayer;
+                            printTable(Board.CurrentStatus(RowMatrix.Table));
                             break;
                         }
                         case 8:
                         {
                             Console.WriteLine($"You entered {input}");
-                            Board.Matrix[2][1] = CurrentPlayer;
-                            printTable(Board.CurrentStatus);
+                            RowMatrix.Table[2][1] = CurrentPlayer;
+                            printTable(Board.CurrentStatus(RowMatrix.Table));
                             break;
                         }
                         case 9:
                         {
                             Console.WriteLine($"You entered {input}");
-                            Board.Matrix[2][2] = CurrentPlayer;
-                            printTable(Board.CurrentStatus);
+                            RowMatrix.Table[2][2] = CurrentPlayer;
+                            printTable(Board.CurrentStatus(RowMatrix.Table));
                             break;
                         }
                     }
