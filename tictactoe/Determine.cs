68a1f6dcb0b64d48cfb73535d4e0eed16b8c8725
Trey Mosley <iiimosley@gmail.com>
2018-06-04 01:07:41 -0500

    winning matrix expression refactor

--- tictactoe/Determine.cs
+++ tictactoe/Determine.cs
@@ -6,19 +6,21 @@ namespace tictactoe
 {
   class Determine
   {
-    static char[][] WinningRows = new char[8][]
+    static char[][] WinningRows (char[][] Table)
     {
-      Matrix.Table[0],
-      Matrix.Table[1],
-      Matrix.Table[2],
-      new char[] { Matrix.Table[0][0], Matrix.Table[1][0], Matrix.Table[2][0] },
-      new char[] { Matrix.Table[0][1], Matrix.Table[1][1], Matrix.Table[2][1] },
-      new char[] { Matrix.Table[0][2], Matrix.Table[1][2], Matrix.Table[2][2] },
-      new char[] { Matrix.Table[0][0], Matrix.Table[1][1], Matrix.Table[2][2] },
-      new char[] { Matrix.Table[0][2], Matrix.Table[1][1], Matrix.Table[2][0] },
-    };
+      return new char[][] {
+        Table[0],
+        Table[1],
+        Table[2],
+        new char[] { Table[0][0], Table[1][0], Table[2][0] },
+        new char[] { Table[0][1], Table[1][1], Table[2][1] },
+        new char[] { Table[0][2], Table[1][2], Table[2][2] },
+        new char[] { Table[0][0], Table[1][1], Table[2][2] },
+        new char[] { Table[0][2], Table[1][1], Table[2][0] },
+      };
+    }
 
-    public static bool Winner(char player) => WinningRows.Any(row => row.All(val => val == player));
+    public static bool Winner(char player) => WinningRows(Matrix.Table).Any(row => row.All(val => val == player));
 
   }
 }
