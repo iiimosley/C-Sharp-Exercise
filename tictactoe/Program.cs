3716f9ce400e9f298fb83489c5bec49535f69d2f
Trey Mosley <iiimosley@gmail.com>
2018-06-03 22:38:18 -0500

    do while refactor

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -5,87 +5,39 @@ namespace tictactoe
     class Program
     {
         public static char CurrentPlayer = 'X';
-        public static void printTable(string status)
-        {
-            Console.WriteLine(status);
-            Console.WriteLine($"Current Player: {CurrentPlayer}");
-            Console.Write("Mark a spot (1-9): ");
-        }
+        // public static void printTable(int x, int y)
+        // {
+
+        // }
         public static void Main()
         {
             try{
                 int input;
                 do {
-                printTable(Board.CurrentStatus(RowMatrix.Table));
+                Console.WriteLine(Board.CurrentStatus(RowMatrix.Table));
+                Console.WriteLine($"Current Player: {CurrentPlayer}");
+                Console.Write("Mark a spot (1-9): ");
                 input = int.Parse(Console.ReadLine());
                 if(input == 1)
-                {
-                    Console.WriteLine($"You entered {input}");
                     RowMatrix.Table[0][0] = CurrentPlayer;
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                    // input = int.Parse(Console.ReadLine());
-                }
                 else if(input == 2)
-                {
-                    Console.WriteLine($"You entered {input}");
                     RowMatrix.Table[0][1] = CurrentPlayer;
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                    // input = int.Parse(Console.ReadLine());
-                }
                 else if(input == 3)
-                {
-                    Console.WriteLine($"You entered {input}");
                     RowMatrix.Table[0][2] = CurrentPlayer;
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                    // input = int.Parse(Console.ReadLine());
-                }
                 else if(input == 4)
-                {
-                    Console.WriteLine($"You entered {input}");
                     RowMatrix.Table[1][0] = CurrentPlayer;
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                    // input = int.Parse(Console.ReadLine());
-                }
                 else if(input == 5)
-                {
-                    Console.WriteLine($"You entered {input}");
                     RowMatrix.Table[1][1] = CurrentPlayer;
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                    // input = int.Parse(Console.ReadLine());
-                }
                 else if(input == 6)
-                {
-                    Console.WriteLine($"You entered {input}");
                     RowMatrix.Table[1][2] = CurrentPlayer;
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                    // input = int.Parse(Console.ReadLine());
-                }
                 else if(input == 7)
-                {
-                    Console.WriteLine($"You entered {input}");
                     RowMatrix.Table[2][0] = CurrentPlayer;
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                    // input = int.Parse(Console.ReadLine());
-                }
                 else if(input == 8)
-                {
-                    Console.WriteLine($"You entered {input}");
                     RowMatrix.Table[2][1] = CurrentPlayer;
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                    // input = int.Parse(Console.ReadLine());
-                }
                 else if(input == 9)
-                {
-                    Console.WriteLine($"You entered {input}");
                     RowMatrix.Table[2][2] = CurrentPlayer;
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                    // input = int.Parse(Console.ReadLine());
-                }
                 else
-                {
                     Console.WriteLine($"{input} is not a valid input");
-                    //printTable(Board.CurrentStatus(RowMatrix.Table));
-                }
                 }while(input < 10);
             }
             catch(FormatException)
