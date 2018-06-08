a1f668f9ef97b5d42708c045fa420bee13534290
Trey Mosley <iiimosley@gmail.com>
2018-06-07 19:31:47 -0500

    model init for member class

---
+++ AnatomyAPI/AnatomyAPI/Controllers/MemberController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using AnatomyAPI.Models;
+
+namespace AnatomyAPI.Controllers
+{
+    public class MemberController : Controller
+    {
+
+        public ActionResult Detail()
+        {
+
+            var member = new Member()
+            {
+                Name = "Heart",
+                Description = "<p>Keeps the blood flowing and the body moving</p>",
+                Sections = new string[]
+                {
+                    "left ventricle",
+                    "right ventricle",
+                    "left atrium",
+                    "right atrium"
+                }
+            };
+
+            return View(member);
+        }
+    }
+} 
---
+++ AnatomyAPI/AnatomyAPI/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnatomyAPI.Models
+{
+    public class Member
+    {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string[] Sections { get; set; }
+    }
+}
